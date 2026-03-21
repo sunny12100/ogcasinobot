903730d5f9ccc989e7ce8c85bbdd4ae9bb46041a
sunny12100 <sunnygupta123420@gmail.com>
2026-03-22 01:40:12 +0530

    high-low

--- commands/highlow.js
+++ commands/highlow.js
@@ -30,7 +30,7 @@ async function resolveGame({
   isAuto = false,
 }) {
   try {
-    const winChance = 0.35;
+    const winChance = 0.42;
     const shouldWin = randomFloat() < winChance;
 
     let userIndex;
