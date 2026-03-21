f7601e651b54a2957403f3e7b54a85beb29a196d
sunny12100 <sunnygupta123420@gmail.com>
2026-03-22 00:53:37 +0530

    change

--- commands/highlow.js
+++ commands/highlow.js
@@ -141,7 +141,7 @@ Will the next card be **Higher** or **Lower**?`,
 
         setTimeout(async () => {
           try {
-            const winChance = 0.3;
+            const winChance = 0.4;
             const shouldWin = randomFloat() < winChance;
 
             let userIndex;
